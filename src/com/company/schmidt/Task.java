9bd7b1150ba75abbc80e24761506704fe148ef08
Gunnar <gunnarkam621@gmail.com>
2021-04-12 20:49:23 -0400

    Initial commit

---
+++ src/com/company/schmidt/Task.java
@@ -0,0 +1,44 @@
+package com.company.schmidt;
+
+public class Task {
+
+    private String title;
+    private String description;
+    private int priority;
+
+    public Task(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "\tTask " + title + "\n Description: " + description + "\n Priority: " + priority;
+    }
+}
+
